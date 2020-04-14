fdca798220018da5939a32d09db26d261693e01d
tukor11 <55362133+tukor11@users.noreply.github.com>
2020-04-14 15:00:03 +0100

    Update password_validity.py

--- password_validity.py
+++ password_validity.py
@@ -1,20 +1,20 @@
 import re
 
-pattern = '\d+'
-regex = re.compile('[@_!#$&]')
+pattern = "\d+"
+regex = re.compile("[@_!#$&]")
 
 while True:
-    print('Enter your password')
-    pwd = input('>')
+    print("Enter your password")
+    pwd = input(">")
     pwdLength = len(pwd)
     if pwdLength < 8:
-        print('Your password length must be up to 8 characters:')
+        print("Your password length must be up to 8 characters:")
         continue
 
     if regex.search(pwd) == None:
-        print('Your password must contain a number and any of these characters $ # & _ ! @')
+        print("Your password must contain a number and any of these characters $ # & _ ! @")
         continue
 
-    print('Your password is ', pwd)
+    print("Your password is ", pwd)
     break
-print('Password valid')
+print("Password valid")
